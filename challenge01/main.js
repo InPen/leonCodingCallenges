5e820c30f8b1afab0f506f614e3cdc385e1ceb12
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-12 20:56:13 -0500

    Include solution

--- challenge01/main.js
+++ challenge01/main.js
@@ -2,17 +2,20 @@
 //For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
 //sudo code:
-//iterate through numsArray
-//square each number || index
-//gng to use to .map() to get a new array w/ sqRoot values
-//add those values together and return them
+//iterate through our array
+//square each index
+//add every index
 
-const numsArray = [2,4,6]//[1, 2, 2]
-let result = numsArray.map(index => index ** 2)
-console.log(result)
+let array = [1, 2, 2] // 9
 
-let sum = result.reduce(function(index, addValue){
-       return index + addValue
-   }, 0)
-
-   console.log(sum)
+function squareSum(){
+  for(let i = 0; i < array.length; i++) {
+     sqrdElement = Math.pow(array[i], 2)
+     // console.log(sqrdElement)
+     for (let numVal of sqrdElement) {
+       let sumResults = sqrdElement.reduce((a, b) => a + b, 0)
+       console.log(sumResults)
+}
+  }
+}
+squareSum(array)
